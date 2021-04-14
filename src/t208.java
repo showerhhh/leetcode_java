caebdea689eee69bc552129713633446e7d96879
showerhhh <582566236@qq.com>
2021-04-14 16:46:39 +0800

    add

--- src/t208.java
+++ src/t208.java
@@ -1,46 +1,72 @@
 public class t208 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
 
 class Trie {
+    Trie[] children;
+    boolean isEnd;
+
     /**
      * Initialize your data structure here.
      */
     public Trie() {
-
+        children = new Trie[26];
+        isEnd = false;
     }
 
     /**
      * Inserts a word into the trie.
      */
     public void insert(String word) {
-
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
     }
 
     /**
      * Returns if the word is in the trie.
      */
     public boolean search(String word) {
-
+        Trie node = searchPrefix(word);
+        if (node != null && node.isEnd) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * Returns if there is any word in the trie that starts with the given prefix.
      */
     public boolean startsWith(String prefix) {
-
+        Trie node = searchPrefix(prefix);
+        if (node != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    class TreeNode {
-        int[] children;
-        boolean isEnd;
-
-        public TreeNode() {
-            children = new int[26];
-            for (int i = 0; i < children.length; i++) {
-                children[i] = 0;
+    Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
             }
-            isEnd = false;
+            node = node.children[index];
         }
+        return node;
     }
 }
 
