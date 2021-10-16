c70244d8d475e6792358d5c219bfafc3f47253a8
showerhhh <582566236@qq.com>
2021-10-16 19:54:36 +0800

    add

--- src/t面试题_0108.java
+++ src/t面试题_0108.java
@@ -48,7 +48,6 @@ class Solution_t面试题_0108 {
         HashSet<Integer> column = new HashSet<>();
         int m = matrix.length;
         int n = matrix[0].length;
-        int sign = Integer.MAX_VALUE;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -62,6 +61,64 @@ class Solution_t面试题_0108 {
                 if (row.contains(i) || column.contains(j)) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+    }
+
+    public void setZeroes_3(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // 标记第一行和第一列的情况
+        boolean flagRow0 = false, flagColumn0 = false;
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                flagRow0 = true;
+                break;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagColumn0 = true;
+                break;
+            }
+        }
+
+        // 标记其他行和列的情况
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // 重置其他行和列
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 重置第一行和第一列
+        if (flagRow0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (flagColumn0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
